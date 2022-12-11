77e9f6eb0fccaebe1a29768b52da209b82ef88ed
Andre Martinez <andre_martinez15@yahoo.com>
2022-12-11 00:13:56 -0800

    started styling about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,14 +6,15 @@ export default function About() {
   return (
     <div>
       <div className="about-me" id="about">
-      <h1>About</h1>
-      {/* <div className='profile'> */}
+      <div className="about-text">
+      <h1 className='mb-3'>About</h1>
          <img src = {ProfilePicture} alt ="Andre profile" className="profile-pic"></img>
       <p>
         I am a graduate of the UCLA Extension Full-Stack Web Development Bootcamp.  
         I am seeking for opportunities to apply my skillset and grow further as a developer.
         Below are some examples of my work along with the technologies that I am familiar with. 
       </p>
+      </div>
       </div>
     </div>
   );
